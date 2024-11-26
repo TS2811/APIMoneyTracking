58af3a80e6473b7abdc051a3730aea331c3b7d54
TS2811 <S6419410003@sau.ac.th>
2024-11-26 21:02:01 +0700

    Code V.1:API Finish

--- models/money.php
+++ models/money.php
@@ -1,5 +1,5 @@
 <?php
-class user
+class money
 {
     private $connDB;
 
@@ -17,5 +17,45 @@ class user
         $this->connDB = $connectDB;
     }
 
-    
+    public function getMoney($userId)
+    {
+        $query = "SELECT * FROM money_tb WHERE userId = :userId ORDER BY moneyDateTime DESC";
+
+        $userId = intval(htmlspecialchars(strip_tags($userId)));
+
+        $stmt = $this->connDB->prepare($query);
+
+        $stmt->bindParam(':userId', $userId);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function addMoney($moneyDetail, $moneyDateTime, $moneyInOut, $moneyType, $userId)
+    {
+        $query = "
+        INSERT INTO money_tb (moneyDetail, moneyDateTime, moneyInOut, moneyType, userId) 
+        VALUES (:moneyDetail, :moneyDateTime, :moneyInOut, :moneyType, :userId)";
+
+        $moneyDetail = htmlspecialchars(strip_tags($moneyDetail));
+        $moneyDateTime = htmlspecialchars(strip_tags($moneyDateTime));
+        $moneyInOut = htmlspecialchars(strip_tags($moneyInOut));
+        $moneyType = htmlspecialchars(strip_tags($moneyType));
+        $userId = htmlspecialchars(strip_tags($userId));
+
+        $stmt = $this->connDB->prepare($query);
+
+        $stmt->bindParam(':moneyDetail', $moneyDetail);
+        $stmt->bindParam(':moneyDateTime', $moneyDateTime);
+        $stmt->bindParam(':moneyInOut', $moneyInOut);
+        $stmt->bindParam(':moneyType', $moneyType);
+        $stmt->bindParam(':userId', $userId);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
